299f46a6328d8f23f5b90c848a741dc5444a2625
Li Lui <llui1@yahoo.com>
2019-12-04 14:11:52 -0500

    pipes

--- src/app/albums/album-card/album-card.component.ts
+++ src/app/albums/album-card/album-card.component.ts
@@ -13,11 +13,18 @@ export class AlbumCardComponent implements OnInit {
   @Output()
   albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
 
+  newPrice: number;
+
   showAlbum() {
     this.albumClicked.emit(this.album);
   }
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.album.onSale) {
+      // Apply 10% discount
+      this.newPrice = this.album.price - (this.album.price * .10);
+      }
+  }
 }
