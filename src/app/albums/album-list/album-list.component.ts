184f79d9226da77a95d1caa4d728ead64fd3c581
Li Lui <llui1@yahoo.com>
2019-12-04 12:40:28 -0500

    updated

---
+++ src/app/albums/album-list/album-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { Album } from "../album.model";
+import { ALBUMS } from "../albums.data";
+
+@Component({
+  selector: "app-album-list",
+  templateUrl: "./album-list.component.html",
+  styleUrls: ["./album-list.component.css"]
+})
+export class AlbumListComponent implements OnInit {
+  albumsArray: Album[];
+
+  ngOnInit(): void {
+    this.albumsArray = this.albumsArray = ALBUMS;
+  }
+
+  parentFunctionHandler(album) {
+    alert(
+      "Album " + album.albumName + " was sent from the album card component"
+    );
+  }
+}
