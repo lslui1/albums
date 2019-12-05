e9dee4de6c8f296e67fd7bbd0cf994fca021fca0
Li Lui <llui1@yahoo.com>
2019-12-05 13:32:21 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AlbumListComponent } from './albums/album-list/album-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: "", redirectTo: "/welcome", pathMatch: "full" },
+  { path: "about", component: AboutComponent },
+  { path: "welcome", component: WelcomeComponent },
+  { path: "albums", component: AlbumListComponent },
+  { path: "**", component: NotfoundComponent, pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
